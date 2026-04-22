39cfa11cd92afccad01e4f75006af21f86344fda
Bit1717 <peterroman.vonderwahl@student.unisg.ch>
2026-04-22 11:19:19 +0200

    Added changes

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ original_list = [i for i in range(10)]
 
 even_list = return_even(original_list)
 
-st.set_page_config(
-    page_title="FridgeBoss", 
-    layout="wide"
-)
-
 st.write("Hooray, we connected everything")
 
 st.write(even_list)

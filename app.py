3c6668f9408e6041e24e4e5087de698fbe280e96
Bit1717 <peterroman.vonderwahl@student.unisg.ch>
2026-04-22 11:22:43 +0200

    Added a change

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from Feature_01 import return_even
 
+st.set_page_config(
+    page_title="FridgeBoss", 
+    layout="wide")
+
 original_list = [i for i in range(10)]
 
 even_list = return_even(original_list)
